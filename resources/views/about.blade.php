33d023b69a396cb291842311405992702c8f8df8
yahya <yahyaboussakl@gmail.com>
2024-10-22 16:31:19 +0100

    laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+
+    <h1>À propos</h1>
+    <p>Je suis Yahya Bsbs</p>
+
+</div>
