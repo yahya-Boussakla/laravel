29100820d04ea6e7724131a7018e81f5cd8f5e38
yahya <yahyaboussakl@gmail.com>
2024-10-24 16:08:39 +0100

    tuto3

---
+++ lara/resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+<form method="POST" action="">
+    @csrf
+    <div>
+        <label for="name">Nom :</label>
+        <input type="text" name="name" id="name">
+    </div>
+    <div>
+        <label for="email">Email :</label>
+        <input type="email" name="email" id="email">
+    </div>
+    <div>
+        <label for="message">Message :</label>
+        <textarea name="message" id="message"></textarea>
+    </div>
+    <button type="submit">Envoyer</button>
+</form>
